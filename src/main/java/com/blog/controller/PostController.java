c0a96440c1316e5fde334cc8823988757dce86b9
dong3512 <dlehddls1015@naver.com>
2022-09-17 23:11:45 +0900

    첫 커밋

---
+++ src/main/java/com/blog/controller/PostController.java
@@ -0,0 +1,34 @@
+package com.blog.controller;
+
+import com.blog.request.PostCreate;
+import com.blog.response.PostResponse;
+import com.blog.service.PostService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class PostController {
+
+    private final PostService postService;
+
+    @PostMapping("/posts")
+    public void  post(@RequestBody @Valid PostCreate request ){
+        postService.write(request);
+    }
+
+    @GetMapping("/posts/{postId}")
+    public PostResponse get(@PathVariable Long postId) {
+        return postService.get(postId);
+    }
+
+    @GetMapping("/posts")
+    public List<PostResponse> getList() {
+        return postService.getList();
+    }
+}
