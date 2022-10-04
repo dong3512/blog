72e40cc6382a344afbb08e3e57b7a81ec89bf864
dong3512 <dlehddls1015@naver.com>
2022-10-04 23:43:43 +0900

    .

--- src/test/java/com/blog/controller/PostControllerTest.java
+++ src/test/java/com/blog/controller/PostControllerTest.java
@@ -64,29 +64,29 @@ class PostControllerTest {
                 .andDo(print());
     }
 
-    @Test
-    @DisplayName("/posts 요청시 title값은 필수다.")
-    void test2() throws Exception {
-        // given
-        PostCreate request = PostCreate.builder()
-                .content("내용입니다.")
-                .build();
+//    @Test
+//    @DisplayName("/posts 요청시 title값은 필수다.")
+//    void test2() throws Exception {
+//        // given
+//        PostCreate request = PostCreate.builder()
+//                .content("내용입니다.")
+//                .build();
+//
+//        String json = objectMapper.writeValueAsString(request);
+//
+//        //expected
+//        mockMvc.perform(post("/posts")
+//                .contentType(APPLICATION_JSON)
+//                .content(json)
+//        )
+//                .andExpect(status().isBadRequest())
+//                .andExpect(jsonPath("$.code").value("400"))
+//                .andExpect(jsonPath("$.message").value("잘못된 요청입니다."))
+//                .andExpect(jsonPath("$.validation.title").value("타이틀을 입력해주세요."))
+//                .andDo(print());
+//    }
 
-        String json = objectMapper.writeValueAsString(request);
 
-        //expected
-        mockMvc.perform(post("/posts")
-                .contentType(APPLICATION_JSON)
-                .content(json)
-        )
-                .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.code").value("400"))
-                .andExpect(jsonPath("$.message").value("잘못된 요청입니다."))
-                .andExpect(jsonPath("$.validation.title").value("타이틀을 입력해주세요."))
-                .andDo(print());
-    }
-
-    
     @Test
     @DisplayName("/posts 요청시 DB에 값이 저장된다.")
     void test3() throws Exception {
